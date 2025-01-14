c771e7b8cb393922f9d723e54263c762a4fa5faf
Jungnyun Kim <jungnyun.kim@snowflake.com>
2025-01-15 02:04:39 +0900

    Create app.py

---
+++ app.py
@@ -0,0 +1,9 @@
+import streamlit as st
+import openai
+
+# OpenAI API key
+openai.api_key = "open.api_key"
+
+# Streamlit 앱 UI
+st.title("ChatGPT with Streamlit :balloon:")
+st.subheader("OpenAI API in Snowflake")
